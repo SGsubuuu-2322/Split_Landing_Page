3f3e1425e62e50bec04837031b6fb91cf512a909
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-10-31 00:30:27 +0530

    Added the meaningful comments in the script file...

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// These are the constants containing important HTML elements for further modifications...
 const container = document.querySelector(".container");
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 
+// This eventlisteners are for dynamic effects of split...
 left.addEventListener("mouseenter", () =>
   container.classList.add("hover-left")
 );
